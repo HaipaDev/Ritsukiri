7315bc60c3a44c710aff32bc89e3276b90c426dd
Hyper! <hypergamesdev@gmail.com>
2023-06-14 12:17:52 +0200

    14.06.2023 - Improved Consistency
    - Improved Consistency with everything | Taking timeElapsed from FMod Timeline pos
    - Commands, Combo & Fever systems seem to work well
    - Attempts at antimashing system
    - FX mix in FMod for voiced drum hits | Perfect command & Fever new sounds

--- Ritsukiri/Assets/_Project/Scripts/RhythmManager.cs
+++ Ritsukiri/Assets/_Project/Scripts/RhythmManager.cs
@@ -18,13 +18,15 @@ public class RhythmManager : MonoBehaviour{
     [SerializeField,Searchable] public FModEventSound[] drumSounds=new FModEventSound[4];
     [SerializeField,Searchable] public GameObject[] drumImgs=new GameObject[4];
     [SerializeField,Searchable] public Vector2 centerPosForDrumImgs;
-    [SerializeField] public FModEventSound clickSound;
+    [SerializeField] public FModEventSound perfectSound;
+    [SerializeField] public FModEventSound feverSound;
     [ChildGameObjectsOnly][SerializeField] Transform canvasParent;
+    [ChildGameObjectsOnly][SerializeField] Transform drumImgsParent;
     [ChildGameObjectsOnly][SerializeField] Image bgImg;
     [ChildGameObjectsOnly][SerializeField] Image img;
     [SerializeField] Sprite defaultSpr;
     [SerializeField] Sprite lockedSpr;
-    [SerializeField] bool _debugColors;
+    [SerializeField] bool _debug;
 
     [Header("Current Variables")]
     [DisableInEditorMode][SerializeField] bool inputLocked;
@@ -33,39 +35,50 @@ public class RhythmManager : MonoBehaviour{
     [SerializeField] bool commandNotPerfect;
     [SerializeField] int commandNotPerfectCount;
     [SerializeField] int comboStatus;
+    [SerializeField] int mashingCount;
     void Start(){
-        clickSound.eventInstance=RuntimeManager.CreateInstance(clickSound.eventName);
+        perfectSound.eventInstance=RuntimeManager.CreateInstance(perfectSound.eventName);
+        feverSound.eventInstance=RuntimeManager.CreateInstance(feverSound.eventName);
         foreach(FModEventSound s in drumSounds){
             s.eventInstance=RuntimeManager.CreateInstance(s.eventName);
         }
         bgColorDefault=new Color(131f/255f,77f/255f,196f/255f);
+        bgColorFever=new Color(244f/255f,147f/255f,115f/255f);
         bgColorUnlocked=new Color(90f/255f,197f/255f,79f/255f);
         bgColorGood=new Color(252f/255f,239f/255f,141f/255f);
         
         LockInput(false);
         ClearActionHistory();
         SetBGLoop(bgLoopCurrentId);
-        StartCheckingRhythm();
+        StartUpdatingRhythm();
     }
     void Update(){CheckInput();}
     
     Color imgColor=Color.white;Color imgColorAbs=Color.white;
-    float _currentTimeElapsed,_currentTimeElapsedOverflow,_currentTimeElapsedBetweenInputs,_currentTimeElapsedBetweenCommands,_currentTimeElapsedLocked,_lockedTimer;
+    float _timeElapsed,_timeElapsedOverflow,_timeElapsedBetweenInputs,_timeElapsedBetweenCommands,_timeElapsedLocked,_lockedTimer;
     bool _commandExecuting;
     [SerializeField]Color bgColorDefault=new Color(131f/255f,77f/255f,196f/255f);
+    [SerializeField]Color bgColorFever=new Color(244f/255f,147f/255f,115f/255f);
     [SerializeField]Color bgColorUnlocked=new Color(90f/255f,197f/255f,79f/255f);
     [SerializeField]Color bgColorGood=new Color(252f/255f,239f/255f,141f/255f);
-    void FixedUpdate(){bgLoop.eventInstance.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE _state);
+    void FixedUpdate(){
+        bgLoop.eventInstance.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE _state);
         if(_state==FMOD.Studio.PLAYBACK_STATE.PLAYING){
             CheckRhythm();
-            //bgLoop.eventInstance.setParameterByName("Tempo", bpm);
-            float normalizedTime=_currentTimeElapsed/secBetweenBeats;//Debug.Log(_currentTimeElapsed+" | "+normalizedTime);
+            float normalizedTime=_timeElapsed/secBetweenBeats;//Debug.Log(_timeElapsed+" | "+normalizedTime);
             img.color=Color.Lerp(new Color(imgColorAbs.r,imgColorAbs.g,imgColorAbs.b,1), new Color(imgColorAbs.r,imgColorAbs.g,imgColorAbs.b,0), normalizedTime);
             if(!inputLocked){img.sprite=defaultSpr;}else{img.sprite=lockedSpr;imgColorAbs=Color.white;}
-            if(_debugColors){if(!_isInputLocked()){bgImg.color=bgColorUnlocked;}else{bgImg.color=bgColorDefault;}}
-            /*if(hitWindowState==HitWindowState.perfect){bgImg.color=bgColorUnlocked;}
-            else if(hitWindowState==HitWindowState.good){bgImg.color=bgColorGood;}
-            else{bgImg.color=bgColorDefault;}*/
+            if(_debug){
+                if(!_isInputLocked()){
+                    bgImg.color=bgColorUnlocked;
+                    /*if(hitWindowState==HitWindowState.perfect){bgImg.color=bgColorUnlocked;}
+                    else if(hitWindowState==HitWindowState.good){bgImg.color=bgColorGood;}*/
+                }
+                else{
+                    if(comboStatus!=-1){bgImg.color=bgColorDefault;}
+                    else{bgImg.color=bgColorFever;}
+                }
+            }
         }else{SetBGLoop(bgLoopCurrentId);}
     }
     void CheckInput(){
@@ -75,85 +88,93 @@ public class RhythmManager : MonoBehaviour{
         else if(Input.GetKeyDown(KeyCode.S)&&!_isInputLocked()){HitDrum(3);return;}
     }
     void CheckRhythm(){
-        if(_currentTimeElapsed>=secBetweenBeats-0.008f){StartCheckingRhythm();}//CheckRhythm();}//0.008f is good for 120?
-        else{if(_currentTimeElapsed<0){_currentTimeElapsed=0;}_currentTimeElapsed+=Time.fixedDeltaTime;}
+        if(_timeElapsed>=secBetweenBeats-0.008f&&secBetweenBeats>0.008f){StartUpdatingRhythm();}//0.008f is good for 120?
+        else{
+            if(_timeElapsed<0){_timeElapsed=0;}
+            //bgLoop.eventInstance.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE _state);
+            //if(_state==FMOD.Studio.PLAYBACK_STATE.PLAYING){_timeElapsed+=Time.fixedDeltaTime;}else{_timeElapsed=0;}
+            bgLoop.eventInstance.getTimelinePosition(out int _timelinePos);
+            _timeElapsed=(_timelinePos/1000f);
+        }
+        //else{if(_timeElapsed<0){_timeElapsed=0;}_timeElapsed+=Time.fixedDeltaTime;}
 
-        if(_currentTimeElapsedOverflow<0){_currentTimeElapsedOverflow=0;}_currentTimeElapsedOverflow+=Time.fixedDeltaTime;
+        if(_timeElapsedOverflow<0){_timeElapsedOverflow=0;}_timeElapsedOverflow+=Time.fixedDeltaTime;
+        _timeElapsedOverflow=(float)System.Math.Round(_timeElapsedOverflow,3);
+        if(_debug)Debug.Log(_timeElapsed+" | "+_timeElapsedOverflow);
+        if(_timeElapsedOverflow>secBetweenBeats+(secBetweenBeats/2f)){_timeElapsedOverflow=_timeElapsed;}
 
+        ///HitWindow Checking
         hitWindowState=HitWindowState.locked;//float epsilon=0.001f;
-        //if(_currentTimeElapsedOverflow>=secBetweenBeats-0.12f&&_currentTimeElapsedOverflow<=secBetweenBeats+0.4f){hitWindowState=HitWindowState.good;}//0.083f | 0.25f
-        //if(_currentTimeElapsedOverflow>=secBetweenBeats-0.0625f&&_currentTimeElapsedOverflow<=secBetweenBeats+0.25f){hitWindowState=HitWindowState.perfect;}//0.0625f | 0.125f
-        if(_currentTimeElapsedOverflow>=secBetweenBeats-(secBetweenBeats/6)&&_currentTimeElapsedOverflow<=secBetweenBeats+(secBetweenBeats/2)){hitWindowState=HitWindowState.good;}
-        if(_currentTimeElapsedOverflow>=secBetweenBeats-(secBetweenBeats/8)&&_currentTimeElapsedOverflow<=secBetweenBeats+(secBetweenBeats/4)){hitWindowState=HitWindowState.perfect;}
-        //if(Mathf.Abs(secBetweenBeats-_currentTimeElapsedOverflow)<=(secBetweenBeats/4)+epsilon){hitWindowState=HitWindowState.good;}
-        //if(Mathf.Abs(secBetweenBeats-_currentTimeElapsedOverflow)<=(secBetweenBeats/6)+epsilon){hitWindowState=HitWindowState.perfect;}
-        //Debug.Log(_currentTimeElapsed+" | "+_currentTimeElapsedOverflow);
-        if(_currentTimeElapsedOverflow>secBetweenBeats+(secBetweenBeats/2f)){_currentTimeElapsedOverflow=_currentTimeElapsed;}//if(!_isActionHistoryEmpty())ClearActionHistory();}
+        //if(_timeElapsedOverflow>=secBetweenBeats-0.12f&&_timeElapsedOverflow<=secBetweenBeats+0.4f){hitWindowState=HitWindowState.good;}//0.083f | 0.25f
+        //if(_timeElapsedOverflow>=secBetweenBeats-0.0625f&&_timeElapsedOverflow<=secBetweenBeats+0.25f){hitWindowState=HitWindowState.perfect;}//0.0625f | 0.125f
+        if(_timeElapsedOverflow>=secBetweenBeats-(secBetweenBeats/6)&&_timeElapsedOverflow<=secBetweenBeats+(secBetweenBeats/2)){hitWindowState=HitWindowState.good;}
+        if(_timeElapsedOverflow>=secBetweenBeats-(secBetweenBeats/8)&&_timeElapsedOverflow<=secBetweenBeats+(secBetweenBeats/4)){hitWindowState=HitWindowState.perfect;}
+        //if(Mathf.Abs(secBetweenBeats-_timeElapsedOverflow)<=(secBetweenBeats/4)+epsilon){hitWindowState=HitWindowState.good;}
+        //if(Mathf.Abs(secBetweenBeats-_timeElapsedOverflow)<=(secBetweenBeats/6)+epsilon){hitWindowState=HitWindowState.perfect;}
 
-        if(inputLocked){
-            if(_currentTimeElapsedLocked>=_lockedTimer-0.04f){
-                LockInput(false);ClearActionHistory();_currentTimeElapsedLocked=0;_commandExecuting=false;
-            }else{_currentTimeElapsedLocked+=Time.fixedDeltaTime;}
-        }else{_currentTimeElapsedLocked=0;}
+        if(inputLocked){//Unlocking
+            if(_timeElapsedLocked>=_lockedTimer-0.04f){
+                LockInput(false);ClearActionHistory();_timeElapsedLocked=0;if(_timeElapsedBetweenCommands==0)_commandExecuting=false;
+            }else{_timeElapsedLocked+=Time.fixedDeltaTime;}
+        }else{_timeElapsedLocked=0;}
 
-        if(!_isActionHistoryEmpty()&&!_isActionHistoryFull()){
-            if(_currentTimeElapsedBetweenInputs>=(secBetweenBeats+(secBetweenBeats/2))+0.04f){
-                LockInput(true,(secBetweenBeats+(secBetweenBeats/2)));ClearActionHistory();_currentTimeElapsedBetweenInputs=0;
-                commandNotPerfect=false;commandNotPerfectCount=0;comboStatus=0;
-            }else{_currentTimeElapsedBetweenInputs+=Time.fixedDeltaTime;}
-        }else if(_isActionHistoryEmpty()){_currentTimeElapsedBetweenInputs=0;}
+        if(!_isActionHistoryEmpty()&&!_isActionHistoryFull()){///Time after a single drum before reset
+            if(_timeElapsedBetweenInputs>=(secBetweenBeats+(secBetweenBeats/2))+0.04f){
+                LockInput(true,(secBetweenBeats+(secBetweenBeats/2)));ClearActionHistory();_timeElapsedBetweenInputs=0;
+                commandNotPerfect=false;commandNotPerfectCount=0;comboStatus=0;mashingCount=0;
+            }else{_timeElapsedBetweenInputs+=Time.fixedDeltaTime;}
+        }else if(_isActionHistoryEmpty()){_timeElapsedBetweenInputs=0;}
 
-        if((comboStatus>0||comboStatus==-1)){
-            if(_currentTimeElapsedBetweenCommands>=(5*secBetweenBeats+(secBetweenBeats/2)+0.04f)){
-                LockInput(true,(secBetweenBeats+(secBetweenBeats/2)));ClearActionHistory();_currentTimeElapsedBetweenCommands=0;
-                commandNotPerfect=false;commandNotPerfectCount=0;comboStatus=0;
-            }else{_currentTimeElapsedBetweenCommands+=Time.fixedDeltaTime;}
-        }else{_currentTimeElapsedBetweenCommands=0;}
+        if(_commandExecuting&&(comboStatus>0||comboStatus==-1)){///After a successful command, count before resetting combo etc
+            if(_timeElapsedBetweenCommands>=(6*secBetweenBeats+(secBetweenBeats/2)+0.04f)){
+                LockInput(true,(secBetweenBeats+(secBetweenBeats/2)));ClearActionHistory();_timeElapsedBetweenCommands=0;
+                commandNotPerfect=false;commandNotPerfectCount=0;comboStatus=0;mashingCount=0;
+            }else{_timeElapsedBetweenCommands+=Time.fixedDeltaTime;}
+        }else{_timeElapsedBetweenCommands=0;}
     }
-    void StartCheckingRhythm(){
-        //bgLoop=bgLoops[bgLoopCurrentId];
-        //bpm=bgLoop.bpm;
+    void StartUpdatingRhythm(){
         secBetweenBeats=(60/bpm);
         beatsPerSec=(bpm/60);
         imgColorAbs=Color.white;
-        _currentTimeElapsed=0;
+        _timeElapsed=0;
     }
 
     void HitDrum(int i){
         foreach(FModEventSound ds in drumSounds){ds.eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);}
         drumSounds[i].eventInstance.start();
         drumSounds[i].eventInstance.setParameterByName("GoodHitWindow", hitWindowState==HitWindowState.perfect ? 0 : 1);
-        if(hitWindowState==HitWindowState.good){imgColorAbs=Color.yellow;commandNotPerfect=true;}else{imgColorAbs=Color.cyan;}
+        if(hitWindowState==HitWindowState.good){commandNotPerfect=true;imgColorAbs=Color.yellow;}else{imgColorAbs=Color.cyan;}
 
-        var _drumImg=Instantiate(drumImgs[i],canvasParent);Destroy(_drumImg,0.5f);
+        VisualizeDrumHit(i);
+        SetActionHistory(i);
+    }
+    void VisualizeDrumHit(int i){
+        var _drumImg=Instantiate(drumImgs[i],drumImgsParent);Destroy(_drumImg,0.5f);
         float _x=0;float _y=0;
         switch(i){
             case 0:
                 _x=-50;
-                _y=0;
             break;
             case 1:
                 _x=+50;
-                _y=0;
             break;
             case 2:
-                _x=0;
                 _y=+50;
             break;
             case 3:
-                _x=0;
                 _y=-50;
             break;
         }
         Vector2 _random=new Vector2(Random.Range(-10f,10f),Random.Range(-10f,10f));
-        _drumImg.transform.localPosition=centerPosForDrumImgs+new Vector2(_x,_y)+_random;
-
-        SetActionHistory(i);
+        _drumImg.transform.localPosition=(Vector2)drumImgsParent.transform.localPosition+new Vector2(_x,_y)+_random;
     }
     void SetActionHistory(int id){
         for(var i=0;i<actionHistory.Length;i++){if(actionHistory[i]==-1){actionHistory[i]=id;break;}}
+        if(_timeElapsedBetweenInputs<secBetweenBeats/3&&_timeElapsedBetweenInputs!=0){mashingCount++;}
+        if(mashingCount>2){ClearActionHistory();mashingCount=0;LockInput(true,secBetweenBeats+0.125f);}
+        //if(_timeElapsedBetweenInputs>secBetweenBeats/3||_timeElapsedBetweenInputs==0){for(var i=0;i<actionHistory.Length;i++){if(actionHistory[i]==-1){actionHistory[i]=id;break;}}}else{ClearActionHistory();}
         //if(AreAllValuesEqual(actionHistory)){LockInput(true,(secBetweenBeats+(secBetweenBeats/2)));}
-        _currentTimeElapsedBetweenInputs=0;
+        _timeElapsedBetweenInputs=0;_timeElapsedBetweenCommands=0;_commandExecuting=false;
         
         //Commands
         if(actionHistory[0]==0&&actionHistory[1]==0&&actionHistory[2]==0&&actionHistory[3]==1){Debug.Log("Forward");_commandExecuting=true;}//Forward
@@ -163,21 +184,27 @@ public class RhythmManager : MonoBehaviour{
         else if(actionHistory[0]==2&&actionHistory[1]==2&&actionHistory[2]==0&&actionHistory[3]==1){Debug.Log("Defense");_commandExecuting=true;}//Defense
         else if(actionHistory[0]==3&&actionHistory[1]==3&&actionHistory[2]==2&&actionHistory[3]==2){Debug.Log("Jump");_commandExecuting=true;}//Jump
         else if(actionHistory[0]==3&&actionHistory[1]==3&&actionHistory[2]==1&&actionHistory[3]==0){Debug.Log("Special");_commandExecuting=true;}//Special
-        else{if(_isActionHistoryFull()){LockInput(true,(2*secBetweenBeats)-0.125f);}}
+        else{if(_isActionHistoryFull()){LockInput(true,(2*secBetweenBeats)-0.125f);_commandExecuting=false;comboStatus=0;commandNotPerfectCount=0;commandNotPerfect=false;}}
         
         if(_commandExecuting){
-            LockInput(true,((4*secBetweenBeats)-0.125f));
-            if(!commandNotPerfect){clickSound.eventInstance.start();}
+            LockInput(true,((4*secBetweenBeats)));//-(secBetweenBeats/2)));
+            if(!commandNotPerfect){perfectSound.eventInstance.start();}
+            mashingCount=0;
             if(comboStatus>=0){
                 comboStatus++;
                 if(commandNotPerfect){commandNotPerfectCount++;commandNotPerfect=false;}
-                if(comboStatus==3&&commandNotPerfectCount==0){comboStatus=-1;}
-                if(comboStatus==4&&commandNotPerfectCount==1){comboStatus=-1;}
-                if(comboStatus==5&&commandNotPerfectCount>=2){comboStatus=-1;}
+                if(comboStatus==3&&commandNotPerfectCount==0){ActivateFever();}
+                if(comboStatus==4&&commandNotPerfectCount==1){ActivateFever();}
+                if(comboStatus==5&&commandNotPerfectCount>=2){ActivateFever();}
+                if(comboStatus>5){ActivateFever();}
             }
         }
     }
-    void ClearActionHistory(){for(var i=actionHistory.Length-1;i>=0;i--){actionHistory[i]=-1;}imgColorAbs=Color.red;}
+    void ActivateFever(){
+        comboStatus=-1;
+        feverSound.eventInstance.start();
+    }
+    void ClearActionHistory(){for(var i=actionHistory.Length-1;i>=0;i--){actionHistory[i]=-1;}/*imgColorAbs=Color.red;*/}
     bool _isActionHistoryEmpty(){bool _isEmpty=true;for(var i=actionHistory.Length-1;i>=0;i--){if(actionHistory[i]!=-1){_isEmpty=false;}}return _isEmpty;}
     bool _isActionHistoryFull(){bool _isFull=true;for(var i=actionHistory.Length-1;i>=0;i--){if(actionHistory[i]==-1){_isFull=false;}}return _isFull;}
     bool _isInputLocked(){return inputLocked||hitWindowState==HitWindowState.locked;}
@@ -206,15 +233,10 @@ public class RhythmManager : MonoBehaviour{
         bpm=bgLoop.bpm;
         secBetweenBeats=(60/bpm);
         beatsPerSec=(bpm/60);
-        _currentTimeElapsed=0;
+        _timeElapsed=0;
     }
 }
 
-/*[System.Serializable]
-public class actionHistory {
-    public int drumId;
-    public HitWindowState hitWindowState;
-}*/
 [System.Serializable]
 public class FModEventSound {
 	public string eventName;
